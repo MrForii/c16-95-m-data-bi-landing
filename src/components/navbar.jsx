bed145f2e3ce5349788cbe033f96a39cee19cbaf
Rodrigo Valdez <fori2001@hotmail.com>
2024-02-13 11:59:07 -0300

    install tailwind

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='flex'>
+        <div>
+            Title
+        </div>
+
+        <div>
+            <button>
+                Repositorio
+            </button>
+        </div>
+    </div>
+  )
+}
